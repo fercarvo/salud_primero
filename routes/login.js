b3ef00509f5f9d0373ae8f74aad0b27072808fd7
fernando carvajal <efcu93@gmail.com>
2016-08-26 00:11:45 -0500

    un error al cargar /islogin

--- routes/login.js
+++ routes/login.js
@@ -24,13 +24,13 @@ router.post('/login', function(req, res, next){
 		Operario.findOne({correo:req.body.correo}, function(err, user){
 
 			if(err){ //si pasa un error entra aqui
-				res.send(err);
+				return res.send(err);
 			} else {
 				if (bcrypt.compareSync(req.body.clave, user.clave )) { //valida si la clave es correcta
 					req.session.user = user;
-					res.json(user);	
+					return res.json(user);	
 				} else {
-					res.json({error: "usuario o clave incorrecta"});
+					return res.json({error: "usuario o clave incorrecta"});
 				}
 			}
 			
@@ -43,7 +43,7 @@ router.post('/login', function(req, res, next){
 				res.send(err);
 			}
 			req.session.user = user;
-			res.json(user);
+			return res.json(user);
 		});	
 
 	} else if (req.body.selectRol == "Laboratorista") {
@@ -53,7 +53,7 @@ router.post('/login', function(req, res, next){
 				res.send(err);
 			}
 			req.session.user = user;
-			res.json(user);
+			return res.json(user);
 		});
 	}
 });
@@ -62,26 +62,25 @@ router.post('/login', function(req, res, next){
 router.get('/islogin', function(req, res) {
   if (req.session && req.session.user) { // Check if session exists
     // lookup the user in the DB by pulling their email from the session
-    res.send("si existe");
     User.findOne({ email: req.session.user.email }, function (err, user) {
-      if (!user) {
+      if (!user || !err) {
         // if the user isn't found in the DB, reset the session info and
         // redirect the user to the login page
         req.session.reset();
         //res.redirect('/login');
-        req.send({error: "usted no esta logoneado"});
+        return req.send({error: "usted no esta logoneado"});
       } else {
         // expose the user to the template
         res.locals.user = user;
  
         // render the dashboard page
-        res.json({no_error: "usted esta logoneado",user: req.session.user.email});
+        return res.json({no_error: "usted esta logoneado",user: req.session.user.email});
 
         //res.render('dashboard.jade');
       }
     });
   } else {
-    res.json({error: "usted no esta logoneado"});
+    return res.json({error: "usted no esta logoneado"});
   }
 });
 
