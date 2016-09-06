d1ff4fa25c8b1fdfccf0fdd7f0fb6120aaa29346
kanonvintage <guido.duchi@gmial.com>
2016-09-05 23:12:31 -0500

    tengo sueñooooooo

--- public/scripts/operator/appAngular.js
+++ public/scripts/operator/appAngular.js
@@ -1,83 +1,29 @@
 angular.module('appOperator',['ui.router'])
 	.config(function($stateProvider, $urlRouterProvider){
 		$stateProvider
-			.state('regp',{
+			.state('RegistrarPaciente',{
 				url: '/RegistrarPaciente',
-				templateUrl: 'views/operario/regp.html',
+				templateUrl: 'views/operario/registrarPaciente.html',
 				controller:'ctrlRegp'
 			})
-			.state('regm',{
+			.state('RegistrarMuestra',{
 				url: '/RegistrarMuestra',
-				templateUrl: 'views/operario/regm.html',
-                controller:'ctrlRegm'
+				templateUrl: 'views/operario/registrarMuestra.html',
+                        controller:'ctrlRegm'
 			})
-			.state('reportes',{
+			.state('Reportes',{
 				url: '/Reportes',
-				templateUrl: 'views/operario/reportes.html',
-                controller:'ctrlRep'
+				templateUrl: 'views/operario/reportes.html'
 			});
-		$urlRouterProvider.otherwise('regp');
+		$urlRouterProvider.otherwise('RegistrarPaciente');
 	})
-	.factory('comun',function($http){
-		var comun = {}
-		comun.tareas = [];/*{
-			nombre: 'Comprar comida',
-			prioridad: '2'
-		},{
-			nombre: 'Pasear al perro',
-			prioridad: '1'
-		}]*/
-
-		comun.tarea = {};
-
-		/*comun.eliminar = function(tarea){
-			var indice = comun.tareas.indexOf(tarea);
-			comun.tareas.splice(indice, 1);
-		}*/
-
-		/*Seccion de metodos remotos*/
-		comun.getAll = function(){
-			return $http.get('/tareas')
-			.success(function(data){
-				angular.copy(data, comun.tareas);
-				return comun.tareas
-			})
-		};
-
-		comun.add = function(tarea){
-			return $http.post('/tarea', tarea)
-			.success(function(tarea){
-				comun.tareas.push(tarea);
-			})
-		}
-
-		comun.update = function(tarea){
-			return $http.put('/tarea/'+ tarea._id, tarea)
-			.success(function(data){
-				var indice = comun.tareas.indexOf(tarea);
-				comun.tareas[indice] = data;
-			})
-		}
-
-		comun.delete = function(tarea){
-			return $http.delete('/tarea/'+ tarea._id)
-			.success(function(){
-				var indice = comun.tareas.indexOf(tarea);
-				comun.tareas.splice(indice, 1);
-			})
-		}
-
-		return comun;
+	.controller('ctrlRegp',function($scope, $state){
+		
 	})
-	.controller('ctrlRegp',function($scope, $state, comun){
-		init();
-	})
-	.controller('ctrlRegm',function($scope, $state, comun){
+	.controller('ctrlRegm',function($scope, $state){
 	    init();
 	})
-	.controller('ctrlRep',function($scope, $state, comun){
-	    	
-	})
+
 function init(){
     var urlResultados = "json/muestrasRegistradas.json";
     var n=1;
