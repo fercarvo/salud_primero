762649a2925dcd5aa820a930c330710a02164cd1
kanonvintage <guido.duchi@gmial.com>
2016-09-05 23:42:06 -0500

    no judgement can deny

--- public/scripts/patient/appAngular.js
+++ public/scripts/patient/appAngular.js
@@ -16,58 +16,7 @@ angular.module('appPatient',['ui.router'])
 			});
 		$urlRouterProvider.otherwise('centros');
 	})
-	.factory('comun',function($http){
-		var comun = {}
-		comun.tareas = [];/*{
-			nombre: 'Comprar comida',
-			prioridad: '2'
-		},{
-			nombre: 'Pasear al perro',
-			prioridad: '1'
-		}]*/
-
-		comun.tarea = {};
-
-		/*comun.eliminar = function(tarea){
-			var indice = comun.tareas.indexOf(tarea);
-			comun.tareas.splice(indice, 1);
-		}*/
-
-		/*Seccion de metodos remotos*/
-		comun.getAll = function(){
-			return $http.get('/tareas')
-			.success(function(data){
-				angular.copy(data, comun.tareas);
-				return comun.tareas
-			})
-		};
-
-		comun.add = function(tarea){
-			return $http.post('/tarea', tarea)
-			.success(function(tarea){
-				comun.tareas.push(tarea);
-			})
-		}
-
-		comun.update = function(tarea){
-			return $http.put('/tarea/'+ tarea._id, tarea)
-			.success(function(data){
-				var indice = comun.tareas.indexOf(tarea);
-				comun.tareas[indice] = data;
-			})
-		}
-
-		comun.delete = function(tarea){
-			return $http.delete('/tarea/'+ tarea._id)
-			.success(function(){
-				var indice = comun.tareas.indexOf(tarea);
-				comun.tareas.splice(indice, 1);
-			})
-		}
-
-		return comun;
-	})
-	.controller('ctrlCentros',function($scope, $state, comun){
+	.controller('ctrlCentros',function($scope, $state){
 		cargarCentrosMed();
 	})
     
