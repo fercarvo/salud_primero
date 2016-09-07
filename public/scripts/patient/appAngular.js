3f6a4f59a8d5a85e89392afe51ccf910b59ff55f
lhtc9 <ltenecel@fiec.espol.edu.ec>
2016-09-07 18:52:17 -0500

    trabajando en pdfmake

--- public/scripts/patient/appAngular.js
+++ public/scripts/patient/appAngular.js
@@ -301,3 +301,47 @@ function cargarDatos(){
         });
     });
 }
+
+
+
+
+/*angular.module('appExportarToPDF',['ui.router']
+    .controller('exportarToPdf',function($scope)){
+                
+                }*/
+
+(function(){
+    //export html table to pdf, excel and doc format directive
+    var exportTable = function(){
+        var link = function($scope, elm, attr){
+            $scope.$on(‘export-pdf’, function(e, d){
+                elm.tableExport({type:’pdf’, escape:’false’});
+            });
+            $scope.$on(‘export-excel’, function(e, d){
+                elm.tableExport({type:’excel’, escape:false});
+            });
+            $scope.$on(‘export-doc’, function(e, d){
+                elm.tableExport({type: ‘doc’, escape:false});
+            });
+        }
+        return {
+            restrict: ‘C’,
+            link: link
+        }
+    }
+    angular.module(‘CustomDirectives’, [])
+        .directive(‘exportTable’, exportTable);
+})();
+
+//controlador
+$scope.exportAction = function(){
+    switch($scope.export_action){
+        case ‘pdf’: $scope.$broadcast(‘export-pdf’, {});
+            break;
+        case ‘excel’: $scope.$broadcast(‘export-excel’, {});
+            break;
+        case ‘doc’: $scope.$broadcast(‘export-doc’, {});
+            break;
+        default: console.log(‘no event caught’);
+    }
+}
